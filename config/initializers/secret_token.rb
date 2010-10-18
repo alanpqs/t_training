f6ae277d87570a03cfb062b3e6d9217d26582b55
Alan Miles <alan_miles@ajobwelldone.info>
2010-10-18 10:48:05 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TTraining::Application.config.secret_token = '0bbf757c2d2e00d5cdeb937b99c9cdddfa9c3cb9868a7cf716a3ab49042203adf2948388c7e86dfa02e0f76b4c81c8c86204959724f64f94be99634d5674e6fe'
