7961de9d5a8e94b39a56be577d18714b86ff527e
Alan Miles <alan_miles@ajobwelldone.info>
2010-10-20 14:22:17 +0100

    Menu column and header in place

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,6 +5,10 @@ describe PagesController do
 
   before(:each) do
     @base_title = "Tickets for Training"
+    
+    it "should have a left-hand menu_col with a 'widebox'" do
+    #content  
+    end
   end
 
   describe "GET 'home'" do
