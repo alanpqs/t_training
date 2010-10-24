8ffddc1cd18f1eefe3670e79e2f22b9ecfc19f45
Alan Miles <alan_miles@ajobwelldone.info>
2010-10-24 19:21:11 +0100

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,18 @@ class UsersController < ApplicationController
   
   def new
     @title = "Sign Up"
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Tickets for Training!"
+      redirect_to @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
   end
 
 end
