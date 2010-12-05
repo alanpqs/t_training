6a345f479e80801e5593b173280e2a2d564c0819
Alan Miles <alan_miles@ajobwelldone.info>
2010-12-05 11:18:38 +0000

    Cleaned up migrations

--- db/migrate/20101204234749_add_authorized_to_city.rb
+++
@@ -1,9 +0,0 @@
-class AddAuthorizedToCity < ActiveRecord::Migration
-  def self.up
-    add_column :cities, :authorized, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :cities, :authorized
-  end
-end
