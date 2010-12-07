43781508ac3bc9b5290fda200686c0ec316659b9
Alan Miles <alan_miles@ajobwelldone.info>
2010-12-07 19:30:18 +0000

    Vendor home-page link corrected

--- spec/requests/business/business_links_spec.rb
+++ spec/requests/business/business_links_spec.rb
@@ -7,6 +7,12 @@ describe "BusinessLinks" do
     integration_log_in(@user)
   end
   
+  it "should redirect to the 'business/home' page" do
+    visit business_home_path
+    click_link "Home"
+    response.should have_selector('title', :content => "Training provider - home")
+  end
+  
   it "should have a new vendors link" do
     visit business_home_path
     response.should have_selector("a",  :href => new_business_vendor_path,
