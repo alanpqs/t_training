0fd32ead32dd59288f16a21959313eeb798b9214
Alan Miles <alan_miles@ajobwelldone.info>
2011-01-27 17:36:05 +0000

    Small model changes

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -25,7 +25,8 @@ class Issue < ActiveRecord::Base
   composed_of :ticket_price,
   :class_name => "Money",
   :mapping => [%w(cents cents), %w(currency currency_as_string)],
-  :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) }
+  :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+  :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
   
   CONTACT_TYPES = [ "Email", "Phone" ]
                   
--- app/models/item.rb
+++ app/models/item.rb
@@ -30,7 +30,8 @@ class Item < ActiveRecord::Base
   composed_of :price,
   :class_name => "Money",
   :mapping => [%w(cents cents), %w(currency currency_as_string)],
-  :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) }
+  :constructor => Proc.new { |cents, currency| Money.new(cents || 0, currency || Money.default_currency) },
+  :converter => Proc.new { |value| value.respond_to?(:to_money) ? value.to_money : raise(ArgumentError, "Can't convert #{value.class} to Money") }
   
   DAYTIME_TYPES = [ "All day", "Mornings", "Afternoons", "Evenings" ]
   WEEKDAY_TYPES = [ "mon", "tue", "wed", "thu", "fri", "sat", "sun"]
